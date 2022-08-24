56c1e157bb267f7a6196b6a89a1a73c0c1bbf43c
Hyeong1204 <cho4870@naver.com>
2022-08-24 15:08:41 +0900

    유니티 객체 키보드 이동

--- 02_Shooting/Assets/Script/Move.cs
+++ 02_Shooting/Assets/Script/Move.cs
@@ -1,7 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class Move : MonoBehaviour
 {
@@ -24,26 +27,49 @@ public class Move : MonoBehaviour
     /// </summary>
     private void Update()
     {
-        // Vector3 : 백터를 표현하기 위한 구조체. 위치를 표현할 때도 많이 사용한다.
-        // 백터 : 힘의 방향과 크기를 나타내는 단위
+    //    // Vector3 : 백터를 표현하기 위한 구조체. 위치를 표현할 때도 많이 사용한다.
+    //    // 백터 : 힘의 방향과 크기를 나타내는 단위
 
-        //transform.position += (Vector3.left * speed);   // 아래쪽 방향으로 speed 만큼 움직여라(매 프레임마다)
-        //transform.position += new Vector3(1, 0, 0); 
-        //new Vector3(0, 1, 0);   // 위쪽
-        //new Vector3(0, -1, 0);   // 아래쪽
+    //    //transform.position += (Vector3.left * speed);   // 아래쪽 방향으로 speed 만큼 움직여라(매 프레임마다)
+    //    //transform.position += new Vector3(1, 0, 0); 
+    //    //new Vector3(0, 1, 0);   // 위쪽
+    //    //new Vector3(0, -1, 0);   // 아래쪽
 
-        //  Time.deltaTime : 이전 프레임에서 지금 프레임까지 걸린 시간
-        //transform.position += (speed * Time.deltaTime * Vector3.up);  // 아래쪽 방향으로 speed 만큼 움직여라(매 초마다)
+    //    //  Time.deltaTime : 이전 프레임에서 지금 프레임까지 걸린 시간
+    //    //transform.position += (speed * Time.deltaTime * Vector3.up);  // 아래쪽 방향으로 speed 만큼 움직여라(매 초마다)
 
-        //Test_OldInputManager();
+    //    //Test_OldInputManager();
+        
         transform.position += (speed * Time.deltaTime * dir);
 
-        // Big - Little 
-        // sleep
+    //    // Big - Little 
+    //    // sleep
 
-        // input system
-        // Enent-driven(이벤트 드리븐) 방식으로 구현 -> 일이 있을 때만 동작한다. (전력을 아끼기에 적합한 구조)
+    //    // input system
+    //    // Enent-driven(이벤트 드리븐) 방식으로 구현 -> 일이 있을 때만 동작한다. (전력을 아끼기에 적합한 구조)
     }
+
+    public void MoveInput(InputAction.CallbackContext context)
+    {
+       
+            Debug.Log("입력들어옴 - 2");
+            Vector2 inputdir = context.ReadValue<Vector2>();    // 어느방향으로 움직여야 하는지를 입력받음
+            dir = inputdir;
+            Debug.Log(inputdir);
+        
+
+        // Vector2 : 유니티에서 제공하는 구조체(struct). 2차원 백터를 표현하기 위한 구조체 (x,y)
+        // Vector3 : 유니티에서 제공하는 구조체(struct). 3차원 백터를 표현하기 위한 구조체 (x,y, z)
+    }
+
+    public void Fire(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            Debug.Log("발사");
+        }
+    }
+
 
     private void Test_OldInputManager()
     {
