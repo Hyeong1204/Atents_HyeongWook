4b4b91e8f06babeaf9edbd2e3e357331bac3d0fb
Hyeong1204 <cho4870@naver.com>
2022-08-26 10:47:12 +0900

    2교시

--- 02_Shooting/Assets/Script/Player.cs
+++ 02_Shooting/Assets/Script/Player.cs
@@ -116,4 +116,33 @@ public class Player : MonoBehaviour
     }
 
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("OnCollisionEnter2D");
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        Debug.Log("OnCollisionStay2D");
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        Debug.Log("OnCollisionExit2D");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("OnTriggerEnter2D");
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("OnTriggerStay2D");
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("OnTriggerExit2D");
+    }
 }
