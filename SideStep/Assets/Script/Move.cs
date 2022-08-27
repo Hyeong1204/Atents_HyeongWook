ce26ce2183377db6f9cc423a0600f87747934e32
Hyeong1204 <cho4870@naver.com>
2022-08-27 14:01:25 +0900

    피하기 게임 준비

---
+++ SideStep/Assets/Script/Move.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
