44d5557ae745cb15dad97191e25237fd192aaf45
Hyeong1204 <cho4870@naver.com>
2022-08-29 15:43:17 +0900

    6교시

---
+++ 02_Shooting/Assets/Script/AsteroidSpawner.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidSpawner : MonoBehaviour
+{
+    public GameObject Spawner;
+    float maxY = 4.0f;
+    float minY = -4.0f;
+    float SpawnTime = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Spawn()
+    {
+        while(true)
+        {
+        GameObject obj = Instantiate(Spawner, transform.position, Quaternion.identity);
+        obj.transform.Translate(0, Random.Range(minY, maxY), 0);
+            yield return new WaitForSeconds(SpawnTime);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, new(2, 10, 1));
+    }
+
+}
