fb0cd7615d50479e3a231b26f105a6cff605d6a8
Hyeong1204 <cho4870@naver.com>
2022-09-19 10:05:22 +0900

    09.19 - 1교시

--- 03_3D_Basic/Assets/Script/Platform.cs
+++ 03_3D_Basic/Assets/Script/Platform.cs
@@ -4,53 +4,89 @@ using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
-    Transform target;
-    
+    public Transform destiantion;
+    public float moveSpeed = 1.0f;
 
-    public float speed = 3.0f;      // 엘레베이터의 속도
-    private bool moveStart = false;
+    Rigidbody rigid;
 
-    private Vector3 dir;        // 방향
-    private Vector3 golaTrans;      // 도착지점 위치
+    bool isMoveing = false;
 
     private void Awake()
     {
-        target = transform.GetChild(0);
-
-    }
-
-    private void Start()
-    {
-        dir = target.transform.position - transform.position;   //  방향 구하기
-        dir = dir.normalized;    //dir를 정규화   
-        golaTrans = target.transform.position;  // 시작과 동시에 golaTrans의 값을 target의 위치로 만듬
-
-        target.transform.parent = null;         // 자식 지우기
+        rigid = GetComponent<Rigidbody>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))  //플레이어가 들어가면
+        if (other.CompareTag("Player"))
         {
-            if (transform.position.y < target.transform.position.y)     // transform.position.y가 target.transform.position.y 작으면
-            {
-                moveStart = true;       // moveStart를 true로 바꾸기
-            }
+            playerIn = true;
         }
     }
 
-    private void Update()
+    private void OnTriggerExit(Collider other)
     {
-        if (moveStart)  // moveStart가 true면
+        if (other.CompareTag("Player"))
         {
-            transform.Translate(speed * Time.deltaTime * dir); // dir의 방향으로 이동
-
-            if(transform.position.y >= golaTrans.y)     // 자신의 y위치가 golaTrans y위치보다 크면
-            {
-                moveStart = false;      // moveStart를 false로 바꾸기
-            }
-            
+            playerIn = false;
         }
     }
+
+    private void FixedUpdate()
+    {
+        if (playerIn)
+        {
+            rigid.MovePosition(transform.position + moveSpeed * Time.fixedDeltaTime * (destiantion.position - rigid.position).normalized);
+        }
+    }
+
+    //Transform target;
+
+
+    //public float speed = 3.0f;      // 엘레베이터의 속도
+    //private bool moveStart = false;
+
+    //private Vector3 dir;        // 방향
+    //private Vector3 golaTrans;      // 도착지점 위치
+
+    //private void Awake()
+    //{
+    //    target = transform.GetChild(0);
+
+    //}
+
+    //private void Start()
+    //{
+    //    dir = target.transform.position - transform.position;   //  방향 구하기
+    //    dir = dir.normalized;    //dir를 정규화   
+    //    golaTrans = target.transform.position;  // 시작과 동시에 golaTrans의 값을 target의 위치로 만듬
+
+    //    target.transform.parent = null;         // 자식 지우기
+    //}
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject.CompareTag("Player"))  //플레이어가 들어가면
+    //    {
+    //        if (transform.position.y < target.transform.position.y)     // transform.position.y가 target.transform.position.y 작으면
+    //        {
+    //            moveStart = true;       // moveStart를 true로 바꾸기
+    //        }
+    //    }
+    //}
+
+    //private void Update()
+    //{
+    //    if (moveStart)  // moveStart가 true면
+    //    {
+    //        transform.Translate(speed * Time.deltaTime * dir); // dir의 방향으로 이동
+
+    //        if(transform.position.y >= golaTrans.y)     // 자신의 y위치가 golaTrans y위치보다 크면
+    //        {
+    //            moveStart = false;      // moveStart를 false로 바꾸기
+    //        }
+
+    //    }
+    //}
 
 }
