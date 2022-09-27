6f2f74a087a00297c842520677190891f06929b7
Hyeong1204 <cho4870@naver.com>
2022-09-27 12:35:17 +0900

    09.27 - 3교시

--- 03_3D_Basic/Assets/Script/Goalll.cs
+++ 03_3D_Basic/Assets/Script/Goalll.cs
@@ -1,20 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
 
 public class Goalll : MonoBehaviour
 {
+    public Action onGoalIn;
+
     ParticleSystem[] ps;
     Transform effect;
 
     private void Awake()
     {
         effect = transform.GetChild(2);
-        ps = new ParticleSystem[effect.childCount];
-        for (int i = 0; i < effect.childCount; i++)
-        {
-            ps[i] = effect.GetChild(i).GetComponent<ParticleSystem>();
-        }
+        ps = effect.GetComponentsInChildren<ParticleSystem>();      // 골인할 때 터트릴 파티클 시스템 찾기
+        //ps = new ParticleSystem[effect.childCount];
+        //for (int i = 0; i < effect.childCount; i++)
+        //{
+        //    ps[i] = effect.GetChild(i).GetComponent<ParticleSystem>();
+        //}
     }
 
     private void OnTriggerEnter(Collider other)
@@ -22,14 +27,19 @@ public class Goalll : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             playGoalInEffect();
+            onGoalIn?.Invoke();
         }
     }
 
     void playGoalInEffect()
     {
-        for(int i = 0; i < effect.childCount; i++)
+        foreach(var Effect in ps)       // 찾아놓은 골인 파티클 시스템을 전부 실행하기
         {
-            ps[i].Play();
+            Effect.Play();
         }
+        //for(int i = 0; i < effect.childCount; i++)
+        //{
+        //    ps[i].Play();
+        //}
     }
 }
---
+++ 03_3D_Basic/Assets/Script/Ui/Timer.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    TextMeshProUGUI timeText;
+
+    float currentTime = 0.0f;
+    bool isStart = false;
+
+    private void Awake()
+    {
+        timeText = transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        //SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    //private void OnSceneLoaded(Scene arg0, LoadSceneMode arg1)
+    //{
+    //    StartTimer();
+    //}
+
+    private void Start()
+    {
+        Goalll goal = FindObjectOfType<Goalll>();
+        goal.onGoalIn += StopTimer;
+        StartTimer();
+    }
+
+    float CurrentTime
+    {
+        get => currentTime;
+        set
+        {
+            currentTime = value;
+            timeText.text = $"{currentTime:f2}초";
+        }
+    }
+
+    private void Update()
+    {
+        if (isStart)
+        {
+            CurrentTime += Time.deltaTime;
+        }
+    }
+
+    void StartTimer()
+    {
+        isStart = true;
+        currentTime = 0.0f;
+    }
+
+    void StopTimer()
+    {
+        isStart = false;
+    }
+}
