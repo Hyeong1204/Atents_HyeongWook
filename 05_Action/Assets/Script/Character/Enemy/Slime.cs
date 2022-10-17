15e63802986e7b5863f0f6a76308fb3af6d43a22
Hyeong1204 <cho4870@naver.com>
2022-10-17 13:31:41 +0900

    10.17 - 4교시(2)

--- 05_Action/Assets/Script/Character/Enemy/Slime.cs
+++ 05_Action/Assets/Script/Character/Enemy/Slime.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 [RequireComponent(typeof(Rigidbody))]       // 필수적으로 필요한 컴포넌트가 있을 때 자동으로 넣어주는 유니티 속성
 [RequireComponent(typeof(Animator))]
@@ -23,6 +24,7 @@ public class Slime : MonoBehaviour
 
     Rigidbody rigid;
     Animator anima;
+    NavMeshAgent agent;
 
     /// <summary>
     /// 적의 상태를 나타내기 위한 enum
@@ -47,7 +49,8 @@ public class Slime : MonoBehaviour
         set
         {
             moveTarget = value;
-            lookDir = (moveTarget.position - transform.position).normalized;        // lookDir도 함께 갱신
+            //lookDir = (moveTarget.position - transform.position).normalized;        // lookDir도 함께 갱신
+            //agent.SetDestination(moveTarget.position);
         }
     }
 
@@ -73,11 +76,14 @@ public class Slime : MonoBehaviour
             switch (state)      // 새로운 상태(새로운 상태로 들어가면서 해야할 일 처리
             {
                 case EnemyState.Wait:
+                    agent.isStopped = true;
                     waitTimer = waitTime;           // 타이머 초기화
                     anima.SetTrigger("Stop");       // Idle 애니메이션 재생
                     StateUpdate = Update_Wait;      // FixedUpdate에서 실행될 델리게이트 변경
                     break;
                 case EnemyState.Patrol:
+                    agent.isStopped = false;
+                    agent.SetDestination(moveTarget.position);
                     anima.SetTrigger("Move");       // Move 애니메이션 재생
                     StateUpdate = Update_Patrol;    // FixedUpdate에서 실행될 델리게이트 변경
                     break;
@@ -107,6 +113,7 @@ public class Slime : MonoBehaviour
     {
         rigid = GetComponent<Rigidbody>();
         anima = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
     }
 
     private void Start()
@@ -123,7 +130,7 @@ public class Slime : MonoBehaviour
         }
 
         // 값 초기화 작업
-        State = EnemyState.Patrol;      // 기본 상태 설정(wait)
+        State = EnemyState.Wait;      // 기본 상태 설정(wait)
         anima.ResetTrigger("Stop");     // 트리거가 쌓이는걸 방지
     }
 
@@ -137,16 +144,16 @@ public class Slime : MonoBehaviour
     /// </summary>
     void Update_Patrol()
     {
-            // 이동 처리
-        rigid.MovePosition(transform.position + moveSpeedPerSecond * lookDir);                              // 위치 변경
-        rigid.rotation = Quaternion.Slerp(rigid.rotation, Quaternion.LookRotation(lookDir), 0.2f);          // 이동하는 방향 바라보기
+        // 이동 처리
+        //rigid.MovePosition(transform.position + moveSpeedPerSecond * lookDir);                              // 위치 변경
+        //rigid.rotation = Quaternion.Slerp(rigid.rotation, Quaternion.LookRotation(lookDir), 0.2f);          // 이동하는 방향 바라보기
 
-        if ((transform.position - MoveTarget.position).sqrMagnitude < 0.02f)         // 도착 확인
-        {
-            transform.position = MoveTarget.position;     // 정확한 웨이포인트 지점에 이동 시키기 위해 강제 이동
-            MoveTarget = waypoints.MoveNext();            // 다음 웨이포인트 지점을 MoveTarget으로 설정
-            State = EnemyState.Wait;
-        }
+        //if ((transform.position - MoveTarget.position).sqrMagnitude < 0.02f)         // 도착 확인
+        //{
+        //    transform.position = MoveTarget.position;     // 정확한 웨이포인트 지점에 이동 시키기 위해 강제 이동
+        //    MoveTarget = waypoints.MoveNext();            // 다음 웨이포인트 지점을 MoveTarget으로 설정
+        //    State = EnemyState.Wait;
+        //}
     }
 
     /// <summary>
