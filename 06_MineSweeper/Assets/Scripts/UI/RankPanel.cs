dc36c8349dc8c3dd635c89ddae8663765aeee85a
Hyeong1204 <cho4870@naver.com>
2022-12-12 17:42:00 +0900

    과제 게임이 끝날 때 무조건 하나만 켜지게 하기

--- 06_MineSweeper/Assets/Scripts/UI/RankPanel.cs
+++ 06_MineSweeper/Assets/Scripts/UI/RankPanel.cs
@@ -8,6 +8,13 @@ public class RankPanel : MonoBehaviour
     // 게임이 끝나면 패널 열리기
     // 열릴때 무조건 패널이 하나만 켜져야함
     // 버튼이 눌렸을 때 해당 패널이 열려야함
+
+    Tab[] tabs;
+    private void Awake()
+    {
+        tabs = GetComponentsInChildren<Tab>();
+    }
+
     private void Start()
     {
         GameManager gameManager = GameManager.Inst;
@@ -19,8 +26,11 @@ public class RankPanel : MonoBehaviour
 
     void Open()
     {
-        this.gameObject.SetActive(true);    
-
+        this.gameObject.SetActive(true);
+        if (!tabs[0].gameObject.activeSelf && !tabs[1].gameObject.activeSelf)
+        {
+            tabs[0].ChildPanelOpen();
+        }
     }
 
     void Close()
