73320d23c8855b995986bb9b553f0231b7d99c5a
Hyeong1204 <cho4870@naver.com>
2022-12-27 12:20:51 +0900

    12.27 - 3교시

--- 07_TileMap/Assets/Scripts/Astar/AStar.cs
+++ 07_TileMap/Assets/Scripts/Astar/AStar.cs
@@ -27,14 +27,79 @@ public static class AStar
                 current = open[0];
                 open.RemoveAt(0);
 
-                if(current != goal)
+                if (current != goal)
                 {
+                    // 클로즈 리스트에 넣기
+                    close.Add(current);
 
+                    // 주변 8칸 open 리스트에 넣거나 g값 갱신 시도
+                    for (int y = -1; y < 2; y++)
+                    {
+                        for (int x = -1; x < 2; x++)
+                        {
+                            Node node = gridMap.GetNode(current.x + x, current.y + y);
+                            // 스킵할 것들
+                            if (node == null)    // 맵 박일 경우
+                            {
+                                continue;
+                            }
+
+                            if (node == current) // current는 current의 주변이 아님
+                            {
+                                continue;
+                            }
+
+                            if (node.gridType == Node.GridType.Wall)     // 벽인 경우
+                            {
+                                continue;
+                            }
+
+                            if (close.Exists((x) => x == node))         // 클로즈 리스트에 있는 경우
+                            {
+                                continue;
+                            }
+
+                            bool isDiagonal = Mathf.Abs(x) == Mathf.Abs(y);     // 대각선인지 확인. true면 대각선
+                            if (isDiagonal && (gridMap.GetNode(current.x + x, current.y).gridType == Node.GridType.Wall 
+                                || gridMap.GetNode(current.x, current.y + y).gridType == Node.GridType.Wall))       // 대각선인데 벽에 걸리는 경우
+                            {
+                                continue;
+                            }
+
+                            float distance;
+                            if(isDiagonal)
+                            {
+                                distance = 1.4f;
+                            }
+                            else
+                            {
+                                distance = 1.0f;
+                            }
+
+                            // G값이 current를 거쳐서 가는 것보다 더 작은 경우
+                            if(node.G > current.G + distance)
+                            {
+                                if (node.parent == null)                // 처음 사용 하는 노드다 == 오픈 리스트에도 안들어가 있다.
+                                {
+                                    node.H = GetHeauristic(node, goal); // 휴리스틱 값 계산
+                                    open.Add(node);                     // open리스트에 추가
+                                }
+
+                                node.G = current.G + distance;          // g값 갱신
+                                node.parent = current;                  // 부모 설정(경로)
+                            }
+                        }
+                    }
                 }
                 else
                 {
                     break;
                 }
+            }
+
+            if(current == goal)
+            {
+                // 도작 지점에 도착했다.
             }
         }
 
