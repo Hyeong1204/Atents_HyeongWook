f29a9e32032af2426c6a798f781813c53c39e777
Hyeong1204 <cho4870@naver.com>
2022-12-30 12:21:16 +0900

    12.30 - 3교시

--- 07_TileMap/Assets/Scripts/Spawner/Spawner.cs
+++ 07_TileMap/Assets/Scripts/Spawner/Spawner.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+#if UNITY_EDITOR
+using UnityEditor;
+#endif 
 public class Spawner : MonoBehaviour
 {
     //public GameObject monsterPrefab;    // 생성할 몬스터의 프리팹
@@ -22,11 +24,21 @@ public class Spawner : MonoBehaviour
     /// </summary>
     public GridMap GridMap => gridMap;
 
+    /// <summary>
+    /// 스포너가 있는 씬의 몬스터 메니저
+    /// </summary>
     SceneMonsterManager manager;
+
+    /// <summary>
+    /// 스폰 지역 중에서 벽이 아닌 지역
+    /// </summary>
+    List<Node> spawnAreaList;
+
     private void Start()
     {
         manager = GetComponentInParent<SceneMonsterManager>();
-        gridMap = manager.GridMap;
+        spawnAreaList = manager.CalcSpawnArea(this);                // 스폰 영억 중에서 벽이 아닌 위치들의 모음 가져오기
+        //gridMap = manager.GridMap;
     }
 
     private void Update()
@@ -41,6 +53,22 @@ public class Spawner : MonoBehaviour
             } 
         }
     }
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        Vector3 pos = new Vector3(Mathf.Floor(transform.position.x), Mathf.Floor(transform.position.y));
+        Vector3 p0 = pos;
+        Vector3 p1 = pos + new Vector3(size.x, 0);
+        Vector3 p2 = pos + new Vector3(size.x, size.y);
+        Vector3 p3 = pos + new Vector3(0, size.y);
+
+        Handles.color = Color.yellow;
+        Handles.DrawLine(p0, p1, 5);
+        Handles.DrawLine(p1, p2, 5);
+        Handles.DrawLine(p2, p3, 5);
+        Handles.DrawLine(p3, p0, 5);
+    }
+#endif
 
     public Slime Spawn()
     {
@@ -54,8 +82,7 @@ public class Spawner : MonoBehaviour
                 slime.onDie -= DecressCount;        // DecressCount가 누적되지 않게하기 위한 조치
                 slime.onDie += DecressCount;
             
-                Vector3 spawnPos = manager.GetRandomSpawnPosition(transform.position, size);
-                slime.Initialize(manager.GridMap, spawnPos);
+                slime.Initialize(manager.GridMap, GetSpawnPosition());          // 그리드맵 전달 + 스폰될 위치 전달
             }
         }
 
@@ -65,5 +92,26 @@ public class Spawner : MonoBehaviour
     void DecressCount()
     {
         count--;
+    }
+
+    /// <summary>
+    /// spawnAreaList에서 현재 몬스터가 없는 위치를 랜덤으로 찾는 함수
+    /// </summary>
+    /// <returns>몬스터가 없는 노드의 월드 좌표</returns>
+    Vector3 GetSpawnPosition()
+    {
+        List<Node> spawns = new List<Node>();
+        foreach(var node in spawnAreaList)                  // 미리 찾아 놓은 sapwnAreaList 뒤지기
+        {
+            if(node.gridType == Node.GridType.Plain)        // 평지일 경우 
+            {
+                spawns.Add(node);                           // spawns에 저장
+            }
+        }
+
+        int index = Random.Range(0, spawns.Count);
+        Node target = spawns[index];                        // spawns 중에서 랜덤으로 하나 선택
+        Vector2Int gridPos = new Vector2Int(target.x, target.y);
+        return manager.GridMap.GridToWorld(gridPos);        // 선택한 그리드 좌표를 월드 좌표로 변경해서 리턴
     }
 }
